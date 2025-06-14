145afd06bc42c3018f8c21d4791ed2dbc06f05bc
Nick <nicknyler@gmail.com>
2025-06-14 12:19:23 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -4,5 +4,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return  "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route("/print/hello")
+def print_routes():
+    print("hello")
+    return 'hello'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    return '\n'.join(str(i) for i in range(parameter)) + '\n'
+
+@app.route('/math/<int:num1>/<operator>/<int:num2>')
+def math(num1,operator,num2):
+    if operator == '+':
+        return str(num1 + num2)
+    elif operator == '-':
+        return str(num1 - num2)
+    elif operator == '*':
+        return str(num1 * num2)
+    elif operator == 'div':
+        return str(num1 / num2)
+    elif operator == '%':
+        return str(num1 % num2)
+    else:
+        return 'Invalid operator', 400
+
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
